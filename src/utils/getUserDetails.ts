80d1859ac1140be24835b22102a244c8198f0a98
harikumar <harikumar3868@gmail.com>
2023-11-10 17:58:06 +0530

    Rebased

--- src/utils/getUserDetails.ts
+++ src/utils/getUserDetails.ts
@@ -1,8 +1,26 @@
 import axios from "axios";
 export const getUserDetails = async () => {
 	const response = await axios.get(
-		`${import.meta.env.VITE_BACKEND_URL}/user/getDetails`,
+		`${
+			import.meta.env.VITE_BACKEND_URL
+		}/user/getDetails/?userEmail=123@gmail.com`,
 		{ withCredentials: true }
+	);
+	return response.data;
+};
+
+export const getAllDetails = async () => {
+	const response = await axios.get(
+		`${import.meta.env.VITE_BACKEND_URL}/user/getAllDetails`,
+		{ withCredentials: true }
+	);
+	return response.data;
+};
+
+export const searchUsers = async (name: string, department: string) => {
+	const response = await axios.get(
+		`${import.meta.env.VITE_BACKEND_URL}/user/searchUser/`,
+		{ params: { name: name, department: department } }
 	);
 	return response.data;
 };
