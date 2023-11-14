2764964ee9003ecab2482fcda4526ad8a35821c0
Suhail Ahmed <suhailahmed2001sam@gmail.com>
2023-11-14 23:15:08 +0530

    Add Blur + Search Filter

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -3,6 +3,7 @@ import {
   Button,
   Center,
   Group,
+  Loader,
   Radio,
   Select,
   SimpleGrid,
@@ -17,13 +18,16 @@ import {
   IconFilter,
   IconTriangleInvertedFilled,
 } from "@tabler/icons-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { BACKEND_URL, FRONTEND_URL } from "../../../config";
+import { set } from "husky";
 const users = [
   {
+    id: "1",
     imageURL:
       "https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fH",
     name: "John Doe",
-    rollNo: "108120128",
     socials: [
       {
         name: "X",
@@ -46,13 +50,108 @@ for (let i = 0; i < 10; i++) {
   users.push(users[0]);
 }
 
+type Friend = {
+  id: string;
+  name: string;
+  department: string;
+  imageURL: string;
+  socials: {
+    name: string;
+    link: string;
+  }[];
+};
+
 const Search = () => {
   const [activeTab, setActiveTab] = useState<"sort" | "filter">("sort");
   const [sortMethod, setSortMethod] = useState<
     "name-asc" | "name-desc" | "rollNo-asc" | "rollNo-desc"
   >("name-asc");
   const [filterDepartment, setFilterDepartment] = useState<string | null>(null);
-  const [filterSection, setFilterSection] = useState<string | null>(null);
+  const [friends, setFriends] = useState<Friend[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const [name, setName] = useState<string>("");
+  const [visibleFriends, setVisibleFriends] = useState<Friend[]>([]);
+
+  const fetchAllFriends = async (name: string) => {
+    setError(null);
+    setLoading(true);
+    const fetchFriends = async () => {
+      try {
+        const response = await axios.get(
+          `${BACKEND_URL}/user/searchUser?name=${name}`,
+          { withCredentials: true }
+        );
+        const data = await response.data;
+        const friends = data.map((friend: any) => {
+          const socials = [];
+
+          if (friend.x) {
+            socials.push({
+              name: "X",
+              link: friend.x,
+            });
+          }
+
+          if (friend.linkedin) {
+            socials.push({
+              name: "LinkedIn",
+              link: friend.linkedin,
+            });
+          }
+
+          if (friend.facebook) {
+            socials.push({
+              name: "Facebook",
+              link: friend.facebook,
+            });
+          }
+
+          return {
+            id: friend._id,
+            name: friend.name,
+            department: friend.department,
+            imageURL: BACKEND_URL + "/images/profiles/" + friend.imageURL,
+            socials: socials,
+          };
+        });
+        setFriends(friends);
+        setVisibleFriends(friends);
+      } catch (error: any) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchFriends();
+  };
+
+  useEffect(() => {
+    const sortedFriends = [...friends];
+    switch (sortMethod) {
+      case "name-asc":
+        sortedFriends.sort((a, b) => a.name.localeCompare(b.name));
+        break;
+      case "name-desc":
+        sortedFriends.sort((a, b) => b.name.localeCompare(a.name));
+        break;
+    }
+    setVisibleFriends(sortedFriends);
+  }, [sortMethod]);
+
+  useEffect(() => {
+    let filteredFriends = [...friends];
+    if (filterDepartment) {
+      filteredFriends = filteredFriends.filter(
+        (friend) => friend.department === filterDepartment
+      );
+    }
+    setVisibleFriends(filteredFriends);
+  }, [filterDepartment]);
+
+  useEffect(() => {
+    setVisibleFriends([...friends]);
+  }, [activeTab]);
 
   return (
     <Box className="h-full w-full px-[5%] flex flex-row">
@@ -60,9 +159,24 @@ const Search = () => {
         cols={3}
         className="h-[95%] w-[60%] overflow-y-auto scrollbar-hide"
       >
-        {users.map((user) => (
-          <ProfileCard user={user} />
-        ))}
+        {loading && (
+          <Center className="w-full h-full">
+            <Loader />
+          </Center>
+        )}
+        {error && (
+          <Center className="w-full h-full">
+            <Text className="text-2xl font-bebus">Some Error Occured</Text>
+          </Center>
+        )}
+        {visibleFriends &&
+          (visibleFriends.length === 0 && !loading ? (
+            <Center className="w-full h-full">
+              <Text className="text-2xl font-bebus">Search for Friends</Text>
+            </Center>
+          ) : (
+            visibleFriends.map((friend) => <ProfileCard user={friend} />)
+          ))}
       </SimpleGrid>
       <Center
         style={{
@@ -73,15 +187,21 @@ const Search = () => {
         <Center className="h-[90%] w-[90%] flex flex-col">
           <Center className="w-full h-[20%] items-start">
             <TextInput
+              value={name}
+              onChange={(event) => setName(event.currentTarget.value)}
               classNames={{
                 root: "w-full",
                 input:
                   "bg-transparent w-full rounded-full text-3xl font-bebus border-[2px] border-black py-2 px-5 pl-20 h-[70px] placeholder:text-black placeholder:opacity-25",
                 section: "bg-transparent w-[100px] pr-4",
               }}
-              leftSectionPointerEvents="none"
               leftSection={
-                <IconSearch opacity={0.7} color="#000000" size={40} />
+                <IconSearch
+                  onClick={() => fetchAllFriends(name)}
+                  opacity={0.7}
+                  color="#000000"
+                  size={40}
+                />
               }
               placeholder="SEARCH FOR FRIENDS"
             />
@@ -147,26 +267,6 @@ const Search = () => {
                     label="Name - Descending"
                   />
                 </Center>
-                <Center className="w-fullo justify-start h-[75px]">
-                  <Radio
-                    classNames={{
-                      label: "text-2xl font-fira",
-                      body: "flex flex-row items-center",
-                    }}
-                    value="rollNo-asc"
-                    label="Roll No. - Ascending"
-                  />
-                </Center>
-                <Center className="w-full justify-start h-[75px]">
-                  <Radio
-                    classNames={{
-                      label: "text-2xl font-fira",
-                      body: "flex flex-row items-center",
-                    }}
-                    value="rollNo-desc"
-                    label="Roll No. - Descending"
-                  />
-                </Center>
               </Radio.Group>
             </Tabs.Panel>
             <Tabs.Panel
@@ -192,25 +292,6 @@ const Search = () => {
                   }
                   placeholder="Department"
                   data={["CSE", "ECE", "EEE", "ME", "CE", "CHE", "MME", "PE"]}
-                />
-                <Select
-                  value={filterSection}
-                  onChange={(value) => setFilterSection(value as any)}
-                  classNames={{
-                    wrapper: "rounded-full px-5 py-3 border-[2px] border-black",
-                    input: "text-3xl font-fira placeholder:text-black",
-                    section: "text-black pr-4",
-                    dropdown:
-                      "rounded-b-lg translate-y-3 border-[2px] border-black bg-[#e7e6b6]",
-                    option:
-                      "text-2xl font-fira hover:bg-[#f5f5bd] hover:text-black",
-                  }}
-                  variant="unstyled"
-                  rightSection={
-                    <IconTriangleInvertedFilled size={20} color="#000000" />
-                  }
-                  placeholder="Section"
-                  data={["A", "B"]}
                 />
               </SimpleGrid>
             </Tabs.Panel>
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,28 +1,28 @@
 import { Landing, Home, Login, Search } from "../pages";
 
 export const routes: RouteType[] = [
-	{
-		path: "",
-		element: <Landing />,
-		title: "Landing",
-		description: "Landing Page of App",
-	},
-	{
-		path: "/home",
-		element: <Home />,
-		title: "Home",
-		description: "Home Page of App",
-	},
-	{
-		path: "/login",
-		element: <Login />,
-		title: "Login",
-		description: "Login Page of App",
-	},
-	{
-		path: "search",
-		element: <Search />,
-		title: "Search",
-		description: "Search for your batchmates!",
-	},
+  {
+    path: "",
+    element: <Landing />,
+    title: "Landing",
+    description: "Landing Page of App",
+  },
+  {
+    path: "home",
+    element: <Home />,
+    title: "Home",
+    description: "Home Page of App",
+  },
+  {
+    path: "login",
+    element: <Login />,
+    title: "Login",
+    description: "Login Page of App",
+  },
+  {
+    path: "search",
+    element: <Search />,
+    title: "Search",
+    description: "Search for your batchmates!",
+  },
 ];
